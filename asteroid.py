cc6bb3ec01a5eb834cc6f4f8004d7c6cb706536d
walatch <jakub.pociask@gmail.com>
2024-09-16 18:31:33 +0200

    push of a asteroid game prject

---
+++ asteroid.py
@@ -0,0 +1,29 @@
+import pygame
+import random
+from constants import *
+from circleshape import CircleShape
+import random
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        a1_angle = self.velocity.rotate(random_angle)
+        a2_angle = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = a1_angle * 1.2
+        a2.velocity = a2_angle * 1.2
